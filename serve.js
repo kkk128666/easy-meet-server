1bc38cb158e7ffb39abef5d9bee0e35edb69444d
kkk128666 <qdkc123@163.com>
2025-04-11 21:50:20 +0800

    Add files via upload

---
+++ serve.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" }
+});
+
+io.on("connection", (socket) => {
+  console.log("New connection: " + socket.id);
+
+  socket.on("join-room", (roomId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-joined", socket.id);
+  });
+
+  socket.on("signal", ({ roomId, data }) => {
+    socket.to(roomId).emit("signal", { from: socket.id, data });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected: " + socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
